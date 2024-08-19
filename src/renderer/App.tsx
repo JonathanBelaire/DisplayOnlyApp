92156ad9b02b7a5d95a529e5c1b0245bdd18ad2c
Jonathan Belaire <jonathanbelaire94@gmail.com>
2024-08-19 13:53:43 -0500

    Updated landing page

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,30 +8,30 @@ function Hello() {
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <h1>Display Only</h1>
       <div className="Hello">
         <a
-          href="https://electron-react-boilerplate.js.org/"
+          href="https://displayonly.band"
           target="_blank"
           rel="noreferrer"
         >
           <button type="button">
             <span role="img" aria-label="books">
-              📚
+              🗣️
             </span>
-            Read our docs
+           Social
           </button>
         </a>
         <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
+          href="https://displayonly.band"
           target="_blank"
           rel="noreferrer"
         >
           <button type="button">
             <span role="img" aria-label="folded hands">
-              🙏
+              💰
             </span>
-            Donate
+            Venmo
           </button>
         </a>
       </div>
